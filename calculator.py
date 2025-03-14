390c570c54f1cddf73669b8615483c4181ca290c
savage5675 <rkaguri@kabarak.ac.ke>
2025-03-14 22:17:50 +0300

    updated calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def calculator():
+    num1 = 34
+    num2 = 72
+    operation = '*'
+       if operation == '*':
+        print(f"{num1} * {num2} = {num1 * num2}")
+       else:
+        print("Invalid operation or division by zero.")
+
+calculator()
---
+++ lesson1.py
@@ -0,0 +1,7 @@
+print("hello world, this is session 3")
+
+name = "Anderson Shalom"
+print(name)
+
+students = ["John", "James", "Grace", 3, True]
+print(students)
